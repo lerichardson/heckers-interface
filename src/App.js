afaf5a3469708009f4dd9a35c4ff122387b19044
Levi Richardson <litbelb1@outlook.com>
2021-12-20 19:44:10 -0800

    ✨ add links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import { Card } from "./components/card.js";
+import "./styles/links.css";
+import { GitHub, Twitter, Youtube } from 'react-feather';
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <div className="card-holder">
+        <Card />
+      </div>
+      <div className="App-links">
+        <div className="gh">
+          <a href="https://github.com/lerichardson/heckers-interface" className="link">
+            <GitHub size={24} />
+          </a>
+        </div>
+        <div className="twttr">
+          <a href="https://twitter.com/Litbelb_" className="link">
+            <Twitter size={24} />
+          </a>
+        </div>
+        <div className="yt">
+          <a href="https://www.youtube.com/c/Beluga1" className="link">
+            <Youtube size={24} />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
